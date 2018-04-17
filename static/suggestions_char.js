95445ab1165cdaca36333c434c8f0cff6df3d009
Arne Newman <anewman@retechadvisors.com>
2018-04-16 20:31:52 -0400

    draft of style classifier model

---
+++ static/suggestions_char.js
@@ -0,0 +1,35 @@
+
+//Get references to body elements
+var $color_input = document.querySelector("#color-input");
+var $abv_input = document.querySelector("#abv-input");
+var $ibu_input = document.querySelector("#ibu-input");
+var $method_input = document.querySelector("#method-input");
+
+
+// Add an event listener to the search button, call a function to 
+$searchBtn.addEventListener("click", handleCharButtonClick);
+
+
+//Write the function that drives what occurs with user input
+function handleCharButtonClick() {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//NOTE: BElow is HTML that will direct to a new route on an onclick event
+//<button id="myButton" class="float-left submit-button" >Home</button>
+
+//<script type="text/javascript">
+//    document.getElementById("myButton").onclick = function () {
+//        location.href = "www.yoursite.com";
+//    };
+//</script>
