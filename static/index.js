216f854644f1985803aa6bd8c6c1d1730cf56f72
Shreyas Garga <shreyasgarga@gmail.com>
2018-04-23 20:00:46 -0400

    new working ones

--- static/index.js
+++ static/index.js
@@ -1,48 +1,39 @@
 $(document).ready(function () {
-     var datas = $.get("/style", function (style) {
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:5000/",
-            data: style,
-            dataType: "json",
-            success:
-                $.each(style, function(index,value){
-                    console.log(value);
-                    $("#styles_dropdown").append('<option>'+value+"</option>").selectpicker('refresh')
-                })
+    var datas = $.get("/style", function (style) {
+       $.ajax({
+           type: "GET",
+           url: "http://localhost:5000/",
+           data: style,
+           dataType: "json",
+           success:
+               $.each(style, function(index,value){
+                   console.log(value);
+                   $("#styleDropdown").append('<li><a class="dropdown-item" href="#">'+value+"</a></li>").selectpicker('refresh')
+               })
 
 
-        })
-    }); 
+       })
+   }); 
 
-   /*  var datas = $.get("/names", function (style) {
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:5000/",
-            data: style,
-            dataType: "json",
-            success:
-                $.each(style, function(index,value){
-                    console.log(value);
-                    $("#beers_dropdown").append('<option>'+value+"</option>").selectpicker('refresh')
-                })
-
-
-        })
-    }); */
+  
 })
 
-/* $('select[name="country"]').on('change', function() {
-    var countryId = $(this).val();
+$('#styleDropdown li').click(function(){
+   $('#styles').html($(this).text() + '<span class="caret"></span>')
+   })
 
-    $.ajax({
-        type: "POST",
-        url: "get-province.php",
-        data: {country : countryId },
-        success: function (data) {
-                    //remove disabled from province and change the options
-                    $('select[name="province"]').prop("disabled", false);
-                    $('select[name="province"]').html(data.response);
-        }
-    });
+
+/* $('select[name="country"]').on('change', function() {
+   var countryId = $(this).val();
+
+   $.ajax({
+       type: "POST",
+       url: "get-province.php",
+       data: {country : countryId },
+       success: function (data) {
+                   //remove disabled from province and change the options
+                   $('select[name="province"]').prop("disabled", false);
+                   $('select[name="province"]').html(data.response);
+       }
+   });
 }); */
